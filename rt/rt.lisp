903242e1fe603a5b1893f623aa21684d641c1a04
Philipp Pospischil <philipp.pospischil@univie.ac.at>
2025-05-08 17:51:14 +0200

    feat: add plus minus equals and not equal

--- rt/rt.lisp
+++ rt/rt.lisp
@@ -10,6 +10,41 @@
 (defun not (thingy)
     (if thingy nil t))
 
+(defun + (arg &rest rest)
+    (if (null rest)
+        arg
+        (+-list arg rest)))
+
+(defun +-list (addend list)
+    (if (null list)
+        addend
+        (--list (add-2 addend (car list)) (cdr list))))
+
+(defun - (arg &rest rest)
+    (if (null rest)
+        (sub-2 0 arg)
+        (--list arg rest)))
+
+(defun --list (subtrahend list)
+    (if (null list)
+        subtrahend
+        (--list (sub-2 subtrahend (car list)) (cdr list))))
+
+(defun = (first &rest rest)
+    (=-list first rest))
+
+(defun =-list (first rest)
+    (if (null rest)
+        t
+        (if (nil-if-0 (sub-2 first (car rest)))
+            nil
+            (=-list first (cdr rest)))))
+
+(defun /= (first &rest rest)
+    (not (=-list first rest)))
+
+;;    (let ((result (sub-2 minuend subtrahend))
+;;        (if (null more-subtrahends) result (- result)))))
 (defun concatenate (type &rest strings)
     ;; we assume that the type is 'string without checking
     (concatenate-string-list strings))
